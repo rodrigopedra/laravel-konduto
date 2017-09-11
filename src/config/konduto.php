9b5d1987de89891d5611aab24be6c7ff9a08673a
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2017-09-10 23:39:05 -0300

    remove unused config

--- src/config/konduto.php
+++ src/config/konduto.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'environment' => env( 'KONDUTO_ENVIRONMENT', 'production' ), // 'production' or 'sandbox'
     'debug'       => env( 'KONDUTO_DEBUG', true ), // true ou false indicando se você quer logar os requests
     'public_key'  => env( 'KONDUTO_PUBLIC_KEY', '' ),
     'private_key' => env( 'KONDUTO_PRIVATE_KEY', '' ),
